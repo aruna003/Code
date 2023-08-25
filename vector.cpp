50fcec210389a2f62abf38044210c6976f994825
aruna003 <108563112+aruna003@users.noreply.github.com>
2023-08-25 20:33:09 +0530

    Create vector.cpp

---
+++ vector.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+int main(){
+    vector<int> v;
+    
+    v.push_vack(1);
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(4);
+    
+    // Traversing a vector
+    // Normal way
+    cout<<"Method1"<<endl;
+    for(i=0; i<v.size(); i++){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl<<"Method2 "<<endl;
+    
+    // Using vector iterator. It direcrtly gives the elements.
+    // it is a pointer
+    vector<int>::iterator it;
+    for(it=v.begin(); it!=v.end(); it++){
+        cout<<*it<<endl;
+    }
+    cout<<"Method3"<<endl;
+    // using auto -assigns datatype automatically
+    for(auto element:v){
+        cout<<element<" ";
+    }
+    
+    // pop elements
+    v.pop_back(); 
+}
